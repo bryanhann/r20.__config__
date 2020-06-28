8e8ae2d1bf4b68dd691e22dfb62abb5d27df70a2
Bryan Hann <nobody@nowhere>
2020-06-28 16:28:00 +0800

    Make filestructure shallow

--- activate.sh
+++ activate.sh
@@ -1,5 +1,24 @@
-source ${ZDOTDIR}/functions.sh
-source ${ZDOTDIR}/firstrun.sh
-source ${ZDOTDIR}/hashes.sh
+_block      () { tput setaf $1; [[ "$2" == "." ]] || tput setab $2; shift; shift; echo -n $*; tput sgr0; }
+#_colors     () { for f in $(seq 0 16); do for b in $(seq 0 16); do _block $f $b "$f/$b "; done; echo; done }
+_black      () { _block  15   0  $*; }
+_blue       () { _block  15   4  $*; }
+_yellow     () { _block  16  11  $*; }
+_green      () { _block   0  10  $*; }
+_red        () { _block  15   9  $*; }
+
+_r20_cmd_yes   () { _green "  [exec] $* " ; echo ; $* }
+_r20_cmd_non   () { _green "  [SKIP] $* " ; echo ;    }
+_r20_cmd_bad   () { _red   "  [FAIL] $* " ; echo ;    }
+
+r20export   () { _black "  $1 := [$2] "; echo; export $1=$2; }
+r20source   () { [[ -f $1 ]] && _r20_cmd_yes . $* || _r20_cmd_bad . $* }
+r20clone    () { [ ! -d $2 ] && _r20_cmd_yes git clone $* || _r20_cmd_non clone $* }
+
 export MM=($(ls -1 ${ZDOTDIR} | sort | grep '^[0-9]'))
-for AA in $MM; do r20source ${ZDOTDIR}/${AA}/activate.sh; done
+for AA in $MM; do
+    [[ -d ${ZDOTDIR}/${AA} ]] && r20source ${ZDOTDIR}/${AA}/activate.sh;
+    [[ -f ${ZDOTDIR}/${AA} ]] && r20source ${ZDOTDIR}/${AA};
+done
+
+
+
