fd62c42a7529e10e11ae7f5b65e8410b4d797281
Bryan Hann <nobody@nowhere>
2020-07-13 12:51:45 +0800

    Refactor activate.sh

--- activate.sh
+++ activate.sh
@@ -1,5 +1,4 @@
 _block      () { tput setaf $1; [[ "$2" == "." ]] || tput setab $2; shift; shift; echo -n $*; tput sgr0; }
-#_colors     () { for f in $(seq 0 16); do for b in $(seq 0 16); do _block $f $b "$f/$b "; done; echo; done }
 _black      () { _block  15   0  $*; }
 _blue       () { _block  15   4  $*; }
 _yellow     () { _block  16  11  $*; }
@@ -10,14 +9,12 @@ _r20_cmd_yes   () { _green "  [exec] $* " ; echo ; $* }
 _r20_cmd_non   () { _green "  [SKIP] $* " ; echo ;    }
 _r20_cmd_bad   () { _red   "  [FAIL] $* " ; echo ;    }
 
-r20export   () { _black "  $1 := [$2] "; echo; export $1=$2; }
+r20export   () { _black "  [vset] $1 := [$2] "; echo; export $1=$2; }
 r20source   () { [[ -f $1 ]] && _r20_cmd_yes . $* || _r20_cmd_bad . $* }
 r20clone    () { [ ! -d $2 ] && _r20_cmd_yes git clone $* || _r20_cmd_non clone $* }
 
-export MM=($(ls -1 ${ZDOTDIR} | sort | grep '^[0-9]'))
-for AA in $MM; do
-    [[ -d ${ZDOTDIR}/${AA} ]] && r20source ${ZDOTDIR}/${AA}/activate.sh;
-    [[ -f ${ZDOTDIR}/${AA} ]] && r20source ${ZDOTDIR}/${AA};
+for script in $(ls -1 ${ZDOTDIR}/[0-9]* | sort); do
+    r20source $script
 done
 
 
